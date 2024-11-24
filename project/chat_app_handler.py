eb0354b4dcb647a8e17c9850f4dfc524a7d029ef
mohan <veerubhotla.mohan@gmail.com>
2024-11-24 17:24:34 +0000

    fixed errors

--- project/chat_app_handler.py
+++ project/chat_app_handler.py
@@ -1,8 +1,11 @@
+import io
+from PyPDF2 import PdfReader
 import boto3
 import json
+import base64
 
 from app1 import get_query_response, get_similar_documents, get_vector_store
-from pdf_processor import create_chunks_from_pdf
+from pdf_processor import create_chunks_from_pdf, store_chunks_in_the_db
 
 def send_message(connection_id, endpoint_url, message):
     session = boto3.Session()
@@ -18,11 +21,11 @@ def prompt_handler(event, context):
             connection_id = event['requestContext']["connectionId"]
             endpoint_url = f"https://{event['requestContext']['apiId']}.execute-api.us-east-1.amazonaws.com/dev"
             event_body = json.loads(event["body"])
-            query = event_body("query")
+            query = event_body["query"]
             vector_store =get_vector_store()
             docs = get_similar_documents(query, vector_store)
             response = get_query_response(query, docs)
-            send_message(connection_id, endpoint_url, response)        
+            send_message(connection_id, endpoint_url, response)
         return {
             "statusCode": 200
         }
@@ -32,5 +35,20 @@ def prompt_handler(event, context):
 def pdf_process_handler(event, context):
     print("PDF prcess lambda hit")
     print(event)
-    create_chunks_from_pdf(pdf_path = None)
+    pdf_data = base64.b64decode(event['body'])
 
+    pdf_reader = PdfReader(io.BytesIO(pdf_data))
+    text_content = ""
+    for page in pdf_reader.pages:
+        text_content += page.extract_text()
+    chunks = create_chunks_from_pdf(text_content)
+    vector_store =get_vector_store()
+    store_chunks_in_the_db(chunks, vector_store)
+    return {
+        "statusCode": 200,
+        "headers": {
+            "Access-Control-Allow-Headers" : "Content-Type",
+            "Access-Control-Allow-Origin": "https://www.example.com",
+            "Access-Control-Allow-Methods": "OPTIONS,POST,GET"
+        }
+    }
--- project/pdf_processor.py
+++ project/pdf_processor.py
@@ -1,20 +1,32 @@
 from PyPDF2 import PdfReader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain_core.documents import Document
 
 # method to create chunks from pdf
-def create_chunks_from_pdf(pdf_path):
+def create_chunks_from_pdf(pdf_text):
     try:
-        pdf_reader=PdfReader(pdf_path)
-        text =""
-        for page in pdf_reader.pages:
-            text+=page.extract_text()
+        # pdf_reader=PdfReader(pdf_path)
+        # text =""
+        # for page in pdf_reader.pages:
+        #     text+=page.extract_text()
         text_splitter=CharacterTextSplitter(separator="\n",
                                         chunk_size=1000,
-                                        chunk_overlap=200) 
-                                         
-        chunks=text_splitter.split_text(text)
+                                        chunk_overlap=200)
+
+        chunks=text_splitter.split_text(pdf_text)
         return chunks
 
     except Exception as e:
         print(f"Error processing documents: {e}")
         return ""
+
+
+# method to store chunks  in the vector db using embedings
+def store_chunks_in_the_db(chunks, vector_store):
+    # Create Documents to add to vector store
+    documents = []
+    for chunk in chunks:
+        documents.append(Document(page_content=chunk))
+
+    vector_store.add_documents(documents=documents)
+    print("Chunks stored in vector DB")
